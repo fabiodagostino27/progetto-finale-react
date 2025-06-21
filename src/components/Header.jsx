a1503386409d5ee5bab64f8cf2c1933efac9f79f
fabiodagostino27 <fabiodago27@gmail.com>
2025-06-21 16:01:48 +0200

    Update Header component links to use 'to' prop; enhance HomePage with API data fetching and dynamic rendering of videogames

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -21,13 +21,13 @@ export default function Header() {
                             </NavLink>
                         </li>
                         <li className="nav-item">
-                            <NavLink className="nav-link d-flex align-items-center" href="#">
+                            <NavLink className="nav-link d-flex align-items-center" to={"/videogames"}>
                                 <IoGameController />
                                 <span className="fs-5 ps-1 d-inline d-lg-none">Videogames</span>
                             </NavLink>
                         </li>
                         <li className="nav-item">
-                            <NavLink className="nav-link d-flex align-items-center" href="#">
+                            <NavLink className="nav-link d-flex align-items-center" to={"/platforms"}>
                                 <MdOutlineLaptopChromebook />
                                 <span className="fs-5 ps-1 d-inline d-lg-none">Platforms</span>
                             </NavLink>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,34 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 export default function HomePage() {
+    const [videogames, setVideogames] = useState([]);
+
+    useEffect(() => {
+        axios.get("http://localhost:8080/api/videogames/random")
+            .then((res) => setVideogames(res.data))
+            .catch((err) => console.error("Error fetching videogames:", err));
+    }, []);
+
     return (
-        <h1>Hello World!</h1>
-    )
+      <>
+        <h1 className="fs-1 fw-bold text-center">Welcome to SlowGaming!</h1>
+
+        <h2>Here are some exciting new games!</h2>
+        <div className="m-auto d-flex row">
+            {
+                videogames.map(v => {
+                    return (
+                        <div key={v.id} className="col-4">
+                            <figure className="w-100">
+                                <img className="w-100" src={v.imageUrl} alt={v.title} style={{aspectRatio: '3 / 4'}} />
+                            </figure>
+                            <p>{v.title}</p>
+                        </div>
+                    )
+                })
+            }
+        </div>
+      </>  
+    );
 }
